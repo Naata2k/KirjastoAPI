e4c8e7a4a0cf33ed53f9efa5c0e47b088e78a891
Naata2K <panu.vainio1@gmail.com>
2024-04-16 13:29:00 +0300

    yes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import KirjastoKortti from './components/KirjastoKortti'
 
 function App() {
+  const [hakuTermi, sethakuTermi] = useState('');
+  const [kirjastot, setKirjastot] = useState([]);
+  
+  const kasitteleHaku = async () => {
+
+    //asetetaan kirjastolle ja nimelle erilliset muuttujat, koska voimme hakea kahdella eritavalla kirjastoja
+    const kaupunkiHakuURL = `https://api.kirjastot.fi/v4/library?city.name=${hakuTermi}`;
+    const nimiHakuURL = `https://api.kirjastot.fi/v4/library?name=${hakuTermi}`;
+    
+    //Fetchataan kirjasto ja nimihaulla, ja muutetaan vastaus jsoniksi
+    const kaupunkiHakuVastaus = await fetch(kaupunkiHakuURL);
+    const nimiHakuVastaus = await fetch(nimiHakuURL);
+    
+    const KaupunkiData = await kaupunkiHakuVastaus.json();
+    const NimiData = await nimiHakuVastaus.json();
+
+    console.log(KaupunkiData)   
+
+    if(data.type === "")
+
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      kasitteleHaku();
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+       <div className="haku-container">
+          <input
+            type="text"
+            className="haku-input"
+            placeholder="Etsi kirjaston tai kaupungin nimellä"
+            value={hakuTermi}
+            onChange={(e) => sethakuTermi(e.target.value)}
+            onKeyDown={handleKeyDown}
+          />
+          <button className="haku-nappi" onClick={kasitteleHaku}>Hae</button>
+          {kirjastot.map((kirjasto) => (
+            <KirjastoKortti kirjasto={kirjasto} />
+          ))}
+
+        </div>
+
+
+
     </div>
   );
 }
